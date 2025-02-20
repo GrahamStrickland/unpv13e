30c6705d282f0d80e1b3f2d97499ecc421d7666e
Graham Strickland <grahamhstrickland@gmail.com>
2025-02-20 19:02:17 +0200

    Add count to daytimetcpcli.c

--- intro/daytimetcpcli.c
+++ intro/daytimetcpcli.c
@@ -1,6 +1,7 @@
 #include "unp.h"
 
 int main(int argc, char **argv) {
+  int count;
   int sockfd, n;
   char recvline[MAXLINE + 1];
   struct sockaddr_in servaddr;
@@ -20,7 +21,10 @@ int main(int argc, char **argv) {
   if (connect(sockfd, (SA *)&servaddr, sizeof(servaddr)) < 0)
     err_sys("connect error");
 
+  count = 0;
   while ((n = read(sockfd, recvline, MAXLINE)) > 0) {
+    if (n > 0)
+      count++;
     recvline[n] = 0; /* null terminate */
     if (fputs(recvline, stdout) == EOF)
       err_sys("fputs error");
@@ -28,5 +32,6 @@ int main(int argc, char **argv) {
   if (n < 0)
     err_sys("read error");
 
+  printf("%d read(s) > 0\n", count);
   exit(0);
 }
